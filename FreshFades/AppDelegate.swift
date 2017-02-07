5cc4917d45c509037ab26021a02908e0e7d07c52
Rutger Farry <rutgerfarry@gmail.com>
2017-02-07 14:40:49 -0800

    Add tab bar icons

--- FreshFades/AppDelegate.swift
+++ FreshFades/AppDelegate.swift
@@ -9,9 +9,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         let tabBarController = UITabBarController()
+
         let musicViewController = MusicViewController()
         let scheduleViewController = ScheduleViewController()
         let profileViewController = ProfileViewController()
+
+        musicViewController.tabBarItem = UITabBarItem(
+            title: "Music",
+            image: #imageLiteral(resourceName: "music_tab_bar"),
+            tag: 0)
+
+        scheduleViewController.tabBarItem = UITabBarItem(
+            title: "Schedule",
+            image: #imageLiteral(resourceName: "calendar_tab_bar"),
+            tag: 1)
+
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "Profile",
+            image: #imageLiteral(resourceName: "profile_tab_bar"),
+            tag: 2)
 
         tabBarController.viewControllers = [
             musicViewController,
