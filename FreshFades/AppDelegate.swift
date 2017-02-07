617a72694c868a61db566ce37cccc0754846fe4b
Rutger Farry <rutgerfarry@gmail.com>
2017-02-07 14:03:58 -0800

    Switched from storyboards to XIBs

--- FreshFades/AppDelegate.swift
+++ FreshFades/AppDelegate.swift
@@ -3,27 +3,26 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow? = UIWindow(frame:CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+    var window: UIWindow? = UIWindow()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
         let tabBarController = UITabBarController()
-        let musicViewController = UIStoryboard.music().instantiateInitialViewController()!
-        let scheduleViewController = UIStoryboard.schedule().instantiateInitialViewController()!
-        let profileViewController = UIStoryboard.profile().instantiateInitialViewController()!
-        
-        
+        let musicViewController = MusicViewController()
+        let scheduleViewController = ScheduleViewController()
+        let profileViewController = ProfileViewController()
+
         tabBarController.viewControllers = [
             musicViewController,
             scheduleViewController,
             profileViewController
         ]
-        
+
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
-        
+
         return true
     }
 
 }
-
--- FreshFades/View Controllers/ProfileViewController.swift
+++ FreshFades/View Controllers/ProfileViewController.swift
@@ -1,10 +1,10 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
-    
+
 }
