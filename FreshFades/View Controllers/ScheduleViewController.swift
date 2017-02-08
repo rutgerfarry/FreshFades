e1ca71b567294c94cd05e5c097013ec33b36ea05
Rutger Farry <rutgerfarry@gmail.com>
2017-02-07 23:05:42 -0800

    Outlined entire purchase viewcontroller flow
    Missing pretty much every detail and functionality, but no more view controllers should be needed to implement the flow from: choosing a cut -> choosing a date -> paying and confirming

--- FreshFades/View Controllers/ScheduleViewController.swift
+++ FreshFades/View Controllers/ScheduleViewController.swift
@@ -2,4 +2,14 @@ import UIKit
 
 class ScheduleViewController: UIViewController {
 
+    func appointmentDateAndTimeChosen(date: Date) {
+        let confirmAppointmentViewController = ConfirmAppointmentViewController(date: date)
+        navigationController?.pushViewController(confirmAppointmentViewController, animated: true)
+    }
+
+    @IBAction func makeAppointmentInAnHourButtonPressed(_ sender: Any) {
+        let date = Date(timeIntervalSinceNow: 60*60)
+        appointmentDateAndTimeChosen(date: date)
+    }
+
 }
